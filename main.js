9b8a7b25f51f67e83a18c6b0bb3ac17fb7cca2b7
Christie <olubukolafadipe63@gmail.com>
2023-01-13 20:27:08 -0800

    new update

---
+++ main.js
@@ -0,0 +1,21 @@
+// const switchElement = document.querySelector('.switch')
+
+// switchElement.addEventListener('click', () => {
+//     document.body.classList.toggle('dark')
+// })
+
+// document.querySelector('.switch').addEventListener('click', dark)
+
+// function dark(){
+//     document.body.classList.toggle('dar')
+// }
+
+// document.getElementsByClassName('switch').onclick = makeItDark
+
+// function makeItDark() {
+//     document.querySelector('switch').style.backgroundCOLOR = 'black'
+//
+
+ Mode.onclick = function(){
+    document.body.classList.toggle('dark');
+}
